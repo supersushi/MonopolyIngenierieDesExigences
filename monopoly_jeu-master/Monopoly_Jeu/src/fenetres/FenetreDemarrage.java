836826d511ce27ae84d1daf55cd95367b77eb8bf
MaSql <44373884+MaSql@users.noreply.github.com>
2020-06-26 12:09:54 +0200

    VF fenetre de demarage

--- monopoly_jeu-master/Monopoly_Jeu/src/fenetres/FenetreDemarrage.java
+++ monopoly_jeu-master/Monopoly_Jeu/src/fenetres/FenetreDemarrage.java
@@ -18,6 +18,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -31,10 +32,10 @@ public class FenetreDemarrage {
 
 	private FenetreDeJeu fjeu;
 	private Stage stage;
-	private HBox root;
+	private VBox root;
 	private Label lab;
-	private Label lab2;
-	private ArrayList<TextField> listeJoueurs = new ArrayList<TextField>();
+	private Text lab2;
+	private ArrayList<TextField> listePlayer = new ArrayList<TextField>();
 	private Button butt;
 	private int choix = 0;
 
@@ -53,7 +54,7 @@ public class FenetreDemarrage {
 		this.stage.initOwner(fjeu.getStage());
 		this.stage.initModality(Modality.APPLICATION_MODAL);
 
-		root = new HBox();
+		root = new VBox();
 		initRoot();
 	    Scene scene = new Scene(root, 650.0,650.0);
 		stage.setScene(scene);
@@ -67,12 +68,17 @@ public class FenetreDemarrage {
 		root.setPadding(new Insets(10,10,10,10));
 		root.setSpacing(5);
 
+		lab2 = new Text("MIAGE - Projet Commun ");
+		root.getChildren().add(lab2);
+		lab2.setTranslateX(110);
 		lab = new Label("Joueurs (2 minimum) :");
-		lab2 = new Label("MIAGE - Projet Commun ");
+		HBox box = new HBox();
+		
 		butt = new Button("Commencer !");
 		butt.setOnAction(new EvtValider());
 		butt.setDefaultButton(true);
 		butt.setOnAction(new EvtValider());
+		butt.setTranslateX(160);
 		ImageView iv=new ImageView();
 		iv.setFitHeight(500.0);
 		iv.setFitWidth(500.0);
@@ -80,42 +86,55 @@ public class FenetreDemarrage {
 		iv.setPreserveRatio(true);
 		iv.setImage(new Image("/images/acc.png"));
 		VBox VBox1 =new VBox();
-		//VBox1.setPrefHeight(400.0);
-		VBox1.setPrefWidth(400.0);
-		
+		VBox1.setPrefWidth(300.0);
 		HBox HBox1 =new HBox();
 		HBox1.setPrefHeight(650.0);
-		//HBox1.setPrefWidth(495.);
 		root.setStyle("-fx-background-color: rgba(63, 127, 191, 1);");
-		HBox1.setBlendMode(BlendMode.SRC_OVER);
-		HBox1.setDepthTest(DepthTest.INHERIT);
-		HBox1.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
-		HBox1.setCenterShape(true);
-		lab2.setCenterShape(true);
-
+		
 		HBox.setMargin((iv), new Insets(80,0,0,0));
 
-		
-		VBox1.getChildren().add(lab2);
 		VBox1.getChildren().add(lab);
 		lab.setTranslateY(80);
+		
 		for(int i=0; i<4; i++) {
 			TextField tf = new TextField(i<2?"Player"+(i+1):"");
-			listeJoueurs.add(tf);
-			listeJoueurs.get(i).setPromptText("Player "+(i+1));
-			VBox1.getChildren().add(listeJoueurs.get(i));
-			listeJoueurs.get(i).setPadding(new Insets(10,10,10,10));
-			listeJoueurs.get(i).setTranslateY(150+i*10);
-			
+			listePlayer.add(tf);
+			listePlayer.get(i).setPromptText("Player "+(i+1));
+			VBox1.getChildren().add(listePlayer.get(i));
+			listePlayer.get(i).setPadding(new Insets(10,10,10,10));
+			listePlayer.get(i).setTranslateY(150+i*10);
 		}
-		root.getChildren().add(VBox1);
+		
+		box.getChildren().add(VBox1);
 		
 		VBox1.getChildren().add(butt);
 		butt.setTranslateY(250);
-		lab2.setCenterShape(true);
+		box.getChildren().add(iv);
+		root.getChildren().add(box);
 		
-		HBox1.getChildren().add(iv);
-		root.getChildren().add(HBox1);
+		butt.setStyle(" -fx-background-color:#090a0c,\n" + 
+				"        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n" + 
+				"        linear-gradient(#20262b, #191d22),\n" + 
+				"        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n" + 
+				"    -fx-background-radius: 5,4,3,5;\n" + 
+				"    -fx-background-insets: 0,1,2,0;\n" + 
+				"    -fx-text-fill: white;\n" + 
+				"    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n" + 
+				"    -fx-font-family: \"Arial\";\n" + 
+				"    -fx-text-fill: linear-gradient(white, #d0d0d0);\n" + 
+				"    -fx-font-size: 12px;\n" + 
+				"    -fx-padding: 10 20 10 20;");
+		
+		lab.setStyle("-fx-font-size: 16pt;\n" + 
+				"    -fx-font-family: \"Arial\";\n" + 
+				"    -fx-text-fill: white;\n" + 
+				"    -fx-opacity: 0.6;");
+		lab2.setStyle("-fx-font-size: 30pt;\n" + 
+				"    -fx-font-family: \"Arial\";\n" + 
+				"    -fx-text-fill: white;\n" + 
+				"    -fx-opacity: 0.6;");
+		
+		
 	}
 
 
@@ -139,8 +158,8 @@ public class FenetreDemarrage {
 		public void handle(ActionEvent event) {
 			ArrayList<String> champs = new ArrayList<String>();
 			for(int i=0; i<4; i++) {
-				if(listeJoueurs.get(i).getText() != null && !listeJoueurs.get(i).getText().isEmpty())
-					champs.add(listeJoueurs.get(i).getText());
+				if(listePlayer.get(i).getText() != null && !listePlayer.get(i).getText().isEmpty())
+					champs.add(listePlayer.get(i).getText());
 			}
 			if(champs.size()>=2) {
 				choix = 1;
