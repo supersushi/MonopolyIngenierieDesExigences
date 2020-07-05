49b6c58e4e1bff418a69c4f43fd8ed37129cc9dd
sweat9 <56995613+sweat9@users.noreply.github.com>
2020-07-05 13:24:16 +0200

    commit

--- monopoly_jeu-master/Monopoly_Jeu/src/exception/ViewNotFoundException.java
+++ monopoly_jeu-master/Monopoly_Jeu/src/exception/ViewNotFoundException.java
@@ -1,7 +1,7 @@
 package exception;
 
-public class ResponseNotFoundException extends Exception {
-	public ResponseNotFoundException(String message) {
+public class ViewNotFoundException extends Exception {
+	public ViewNotFoundException(String message) {
 		super(message);
 	}
 }
